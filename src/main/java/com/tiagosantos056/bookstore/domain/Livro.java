98107bbcdc313e9d6ec56ef226263b4bfc5b157a
TiagoSantos056 <tiagosantos0546@gmail.com>
2021-08-30 19:01:35 -0300

    Finalizando as classes de dominio

--- src/main/java/com/tiagosantos056/bookstore/domain/Livro.java
+++ src/main/java/com/tiagosantos056/bookstore/domain/Livro.java
@@ -1,14 +1,30 @@
 package com.tiagosantos056.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+
+@Entity
+public class Livro implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String titulo;
 	private String nome_autor;
 	private String texto;
 	
+	@ManyToMany
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
