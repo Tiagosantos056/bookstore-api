47214be4ba58e7ad52bcdcc430e19121da6573d2
TiagoSantos056 <tiagosantos0546@gmail.com>
2021-09-02 21:48:26 -0300

    Tarefa: Criando Classe TestConfig, Correcoes no Aplication.Properties e Aplication-test.Properties, criacao da Classe DBService

--- src/main/java/com/tiagosantos056/bookstore/BookstoreApplication.java
+++ src/main/java/com/tiagosantos056/bookstore/BookstoreApplication.java
@@ -13,30 +13,13 @@ import com.tiagosantos056.bookstore.repositories.CategoriaRepository;
 import com.tiagosantos056.bookstore.repositories.LivroRepository;
 
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner{
+public class BookstoreApplication {
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
 	
-	@Autowired
-	private LivroRepository livroRepository;
-
 	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-	
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-		
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "O renomado especialista em software, Robert C. Martin, apresenta um paradigma revolucionário com Código limpo: Habilidades Práticas do Agile Software.", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-		
 	}
 
 }
---
+++ src/main/java/com/tiagosantos056/bookstore/service/DBService.java
@@ -0,0 +1,43 @@
+package com.tiagosantos056.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tiagosantos056.bookstore.domain.Categoria;
+import com.tiagosantos056.bookstore.domain.Livro;
+import com.tiagosantos056.bookstore.repositories.CategoriaRepository;
+import com.tiagosantos056.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+
+	
+	public void instanciaBaseDeDados() {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Empreendedorismo", "Livros sobre Negócios e Empresas");
+		Categoria cat3 = new Categoria(null, "Marketing", "Livros de Marketing e Propaganda");
+		Categoria cat4 = new Categoria(null, "Auto ajuda", "Livros de Auto ajuda");
+		Categoria cat5 = new Categoria(null, "Desenvolvimento pessoal", "Livros de Desenvolvimento Pessoal");
+		
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "O renomado especialista em software, Robert C. Martin, apresenta um paradigma revolucionário com Código limpo: Habilidades Práticas do Agile Software.", cat1);
+		Livro l2 = new Livro(null, "Do mil ao Milhão sem cortar o cafézinho", "Thiago Nigro", "Livro sobre investimentos e Negócios.", cat2);
+		Livro l3 = new Livro(null, "PLD", "Pablo Marçal", "Programa Liberdade Digital.", cat3);
+		Livro l4 = new Livro(null, "Sonho Grande", "Jorge Paulo Lemman", "O sonho de construir a maior cervejaria do mundo", cat4);
+		Livro l5 = new Livro(null, "Mais esperto que o diabo", "Napoleon Hill", "O segredo revelado do sucesso", cat1);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		cat2.getLivros().addAll(Arrays.asList(l2));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3, l4, l5));
+	}
+	
+}
